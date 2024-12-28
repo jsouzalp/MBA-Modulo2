0ff7d0ecc1ed970e349f94236f7bfa9141730617
DARKLEGION\Jason <jason.amaral@gmail.com>
2024-12-28 17:32:54 -0300

    Front - Criação de rotinas de autenticação

---
+++ src/front/src/app/services/user.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, catchError, map, Observable } from 'rxjs';
+import { BaseService } from './BaseService';
+import { LoginRecoveryModel } from '../pages/user/models/login-recovery.model';
+import { LoginResetModel } from '../pages/user/models/login-reset.model';
+import { LoginModel } from '../pages/user/models/login.model';
+import { UserTokenModel } from '../pages/user/models/user-token.model';
+
+
+@Injectable({ providedIn: 'root' })
+export class UserService extends BaseService {
+
+  public currentUrl = new BehaviorSubject<any>(undefined);
+
+  constructor(private http: HttpClient) {
+    super();
+  }
+  login(login: LoginModel): Observable<UserTokenModel> {
+    let response = this.http
+      .post(this.UrlServiceV1 + 'v1/user/login', login, this.getHeaderJson())
+      .pipe(
+        map(this.extractData),
+        catchError(this.serviceError));
+
+    return response;
+  }
+
+  passwordRecovery(login: LoginRecoveryModel): Observable<boolean> {
+    let response = this.http
+      .post(this.UrlServiceV1 + `Auth/forgot-password`, login, this.getHeaderJson())
+      .pipe(
+        map(this.extractData),
+        catchError(this.serviceError));
+
+    return response;
+  }
+
+  restorePassword(loginReset: LoginResetModel): Observable<boolean> {
+    let response = this.http
+      .post(this.UrlServiceV1 + `Auth/restore-password`, loginReset, this.getHeaderJson())
+      .pipe(
+        map(this.extractData),
+        catchError(this.serviceError));
+
+    return response;
+  }
+
+}
