85d77d796066eed4957c0ec14b16416f68fb3d36
Hugo Nunes <hgdmaf@gmail.com>
2025-01-20 20:46:38 -0300

    Create DateTimeExtension

---
+++ src/api/FinPlanner360.Business/Extensions/DateTimeExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace FinPlanner360.Business.Extensions
+{
+    public static class DateTimeExtension
+    {
+        public static DateTime GetEndDate(this DateTime value)
+        {
+            return new DateTime(value.Year, value.Month, value.Day, 23, 59, 59);
+        }
+    }
+}
